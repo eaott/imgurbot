308dab2b76ac4122481d70824d0039f4cd41482e
Evan Ott <evan.ott@utexas.edu>
2016-12-23 21:39:56 -0600

    changing logging to be better

--- log.sh
+++ log.sh
@@ -2,3 +2,7 @@
 
 mv /home/pi/logs/imgur.log "/home/pi/logs/`date +%Y_%m_%d_imgur.log`"
 touch /home/pi/logs/imgur.log
+cd /home/pi/GitProjects/imgurbot
+python iscore.py >> iscore.csv
+git add iscore.csv
+git commit -m "adding data to iscore.csv `date`"
